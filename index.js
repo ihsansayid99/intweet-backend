f86e68f01b3015d428af8a22f81357e0bb8f64e5
Ihsan Sayid Muharram <61352696+ihsansayid99@users.noreply.github.com>
2021-05-07 05:46:49 +0700

    fix api

--- index.js
+++ index.js
@@ -1,30 +1,68 @@
-const express = require('express')
+/** require dependencies */
+const express = require("express")
 const cors = require('cors')
+const helmet = require('helmet')
+const path = require('path')
+const puppeteer = require("puppeteer")
+
 const app = express()
-const port = process.env.PORT || 8080
-const screenshot = require('./screenshot')
 
-const whitelist = ['https://intweet.vercel.app/', 'http://localhost:3000']
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
+let port = process.env.PORT || 8080
+
+/** set up middlewares */
+app.use(cors())
+app.use(express.json({limit: '50mb'}))
+app.use(helmet())
+
+app.get("/", (request, response) => {
+    response.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('*', function(req, res) {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
+});
+
+app.post('/api/screenshot', async (req, res, next) => {
+    const { url } = req.body
+    if (!req.body.url) {
+        return res.end('Please specify url like this: ?url=example.com');
     }
-  }
-}
-
-app.get('/', cors(corsOptions), (req, res) => res.status(200).json({ status: 'ok' }))
-
-app.get('/screenshot', cors(corsOptions), (req, res) => {
-  const url = req.query.url
-  ;(async () => {
-    const buffer = await screenshot(url)
-    // res.setHeader('Content-Disposition', 'attachment; filename="screenshot.png"')
-    res.setHeader('Content-Type', 'application/json')
-    res.send(buffer)
-  })()
+    try{
+        const screenshot = await takeScreenshot(url)
+        // res.writeHead(200, {
+        //     'Content-Type': 'application/json',
+        //     'Content-Length': screenshot.length
+        // })
+        res.send({result: screenshot})
+    }catch(err){
+        res.end('error: ' + err.message)
+    }
+    // screenshot.then(response => {
+    //     res.setHeader("Content-Type", "application/json");
+    //     res.send({data: response})
+    // })
+    // res.send({result: screenshot })
+    // next()    
 })
 
-app.listen(port, () => console.log(`app listening on port ${port}!`))
+async function takeScreenshot(url) {
+    const browser = await puppeteer.launch({ 
+       headless: true,
+       args: ['--no-sandbox'] 
+     });
+    const page = await browser.newPage();
+    await page.goto(url, { waitUntil: 'networkidle0' });
+    const screenShot = await page.screenshot({
+        fullPage: true,
+        type: 'png',
+        encoding: 'base64'
+    })
+
+    await browser.close();
+    return screenShot;
+}
+
+/** start server */
+app.listen(port, () => {
+    console.log(`Server started at port: ${port}`);
+});
