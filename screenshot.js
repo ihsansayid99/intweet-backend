57f6dfa5f01f5974d6ca3776ae7fd607d27bcf7e
Ihsan Sayid Muharram <61352696+ihsansayid99@users.noreply.github.com>
2021-05-07 03:15:49 +0700

    first

---
+++ screenshot.js
@@ -0,0 +1,27 @@
+const puppeteer = require('puppeteer')
+
+module.exports = function (url) {
+  return new Promise((resolve, reject) => {
+    ;(async () => {
+      const browser = await puppeteer.launch({
+        // headless: true, // debug only
+        args: ['--no-sandbox']
+      })
+
+      const page = await browser.newPage()
+
+      await page.goto(url, {
+        waitUntil: ['load', 'networkidle0', 'domcontentloaded']
+      })
+
+      const buffer = await page.screenshot({
+        fullPage: true,
+        type: 'png'
+      })
+
+      await browser.close()
+
+      resolve(buffer)
+    })()
+  })
+}
