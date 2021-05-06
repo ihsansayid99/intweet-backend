8f04a41904b5d01790ebcb6bb83209d59a03805e
Ihsan Sayid Muharram <61352696+ihsansayid99@users.noreply.github.com>
2021-05-07 04:11:28 +0700

    add cors

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 8080
 const screenshot = require('./screenshot')
 
-app.get('/', (req, res) => res.status(200).json({ status: 'ok' }))
+const whitelist = ['https://intweet.vercel.app/', 'http://localhost:3000']
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
 
-app.get('/screenshot', (req, res) => {
+app.get('/', cors(corsOptions), (req, res) => res.status(200).json({ status: 'ok' }))
+
+app.get('/screenshot', cors(corsOptions), (req, res) => {
   const url = req.query.url
   ;(async () => {
     const buffer = await screenshot(url)
